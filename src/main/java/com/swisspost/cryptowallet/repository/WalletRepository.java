17501066aea54b60b8634d0c89ab5e71bd49c906
carolinaGoncalves <carolina.goncalves@hotmail.com>
2026-07-22 11:59:50 +0100

    create entities repositiories to communicate with database

---
+++ src/main/java/com/swisspost/cryptowallet/repository/WalletRepository.java
@@ -0,0 +1,15 @@
+package com.swisspost.cryptowallet.repository;
+
+import com.swisspost.cryptowallet.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    @Query("SELECT DISTINCT wallet.symbol FROM Wallet wallet")
+    List<String> findDistinctSymbols();
+}
