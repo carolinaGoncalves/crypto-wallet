7a78b4bab7f2dfeb72e344dec3fc60454b946bda
carolinaGoncalves <carolina.goncalves@hotmail.com>
2026-07-23 07:41:37 +0100

    update scheduler job with the new Wallet and Wallet asset relationship

---
+++ src/main/java/com/swisspost/cryptowallet/service/WalletService.java
@@ -0,0 +1,17 @@
+package com.swisspost.cryptowallet.service;
+
+import com.swisspost.cryptowallet.entity.User;
+import com.swisspost.cryptowallet.entity.Wallet;
+import com.swisspost.cryptowallet.entity.WalletAsset;
+import com.swisspost.cryptowallet.exception.UserNotFoundException;
+import com.swisspost.cryptowallet.repository.UserRepository;
+import com.swisspost.cryptowallet.repository.WalletRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class WalletService {
+
+
+}
+
