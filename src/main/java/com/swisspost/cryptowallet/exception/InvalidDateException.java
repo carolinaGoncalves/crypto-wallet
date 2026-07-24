7261c69ca5dfafab5c1afbdf9047c8b4e61ec9a8
carolinaGoncalves <carolina.goncalves@hotmail.com>
2026-07-24 08:06:08 +0100

    add global exception handler for spring know how to map the exception

---
+++ src/main/java/com/swisspost/cryptowallet/exception/InvalidDateException.java
@@ -0,0 +1,9 @@
+package com.swisspost.cryptowallet.exception;
+
+
+public class InvalidDateException extends RuntimeException {
+
+    public InvalidDateException(String message) {
+        super(message);
+    }
+}
